72ac80cfc567d298a0a6f2c1ffae3e5b34a33195
Nosilv <nosilv8@gmail.com>
2025-02-14 21:30:09 +0900

    first commit: add README.md

---
+++ src/main/java/com/example/sparta_3rd_newsfeed/Sparta3rdNewsfeedApplication.java
@@ -0,0 +1,13 @@
+package com.example.sparta_3rd_newsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sparta3rdNewsfeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sparta3rdNewsfeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sparta_3rd_newsfeed/Sparta3rdNewsfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sparta_3rd_newsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sparta3rdNewsfeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
