9f96227825362e72f3fdca06db205341b75996e7
Nosilv <nosilv8@gmail.com>
2025-02-19 02:36:39 +0900

    comment: API 명세에 사용된 단어로 주석 수정
    fixes: #8

--- src/main/java/com/example/sparta_3rd_newsfeed/feed/service/FeedService.java
+++ src/main/java/com/example/sparta_3rd_newsfeed/feed/service/FeedService.java
@@ -16,26 +16,26 @@ public class FeedService {
 
     private final FeedRepository feedRepository;    // 생성자 자동 생성
 
-    // "R" 전체 게시글 조회
+    // "R" 전체 게시물 조회
     public List<Feed> getAllFeeds() {
         // 모든 Feed 리스트를 가져와서 List<Feed> 형태로 반환
         return feedRepository.findAll();
     }
 
-    // "R" 특정한 (게시글)ID의 게시글만 조회
+    // "R" 단일 게시물 조회
     public Feed getFeedById(Long id) {
         // 인자값을 id로 하여 해당 게시글을 찾고 반환 + 해당 ID의 게시글이 없으면 예외 발생
         return feedRepository.findById(id)
                 .orElseThrow(()-> new IllegalArgumentException("해당 피드가 존재하지 않습니다."));
     }
 
-    // "C" 게시글 생성
+    // "C" 게시물 생성
     public Feed createFeed(Feed feed) {
         // DB에 새로운 게시글 저장 & 게시글 객체 반환
         return feedRepository.save(feed);
     }
 
-    // "U" 특정 게시글 수정
+    // "U" 특정 게시물 수정
     public Feed updateFeed(Long id, Feed newFeed) {
         // 1. 미리 생성한 메소드로 수정하고자 하는 게시글 가져오기
         Feed feed = getFeedById(id);                    // JPA: Persistence Context-REAL
@@ -47,7 +47,7 @@ public class FeedService {
                                                         // 다만, 불필요한 DB 트랜젝션이 발생할 수 있으므로 문제가 된다면 수정하기
     }
 
-    // "D" 특정 게시글 삭제
+    // "D" 특정 게시물 삭제
     public void deleteFeed(Long id) {
         // 1. 미리 생성한 메소드로 삭제하고자 하는 게시글 가져오기
         Feed feed = getFeedById(id);    // JPA: Persistence Context-REAL
